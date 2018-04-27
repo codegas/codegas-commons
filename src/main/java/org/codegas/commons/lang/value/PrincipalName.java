49440d734f9afa212e47e784aaeaf8bcd4d8cbc5
spierce <spierce@homeaway.com>
2018-04-27 13:34:02 -0500

    [SPIKE] [PATCH] Add convenience constructor

--- src/main/java/org/codegas/commons/lang/value/PrincipalName.java
+++ src/main/java/org/codegas/commons/lang/value/PrincipalName.java
@@ -1,5 +1,6 @@
 package org.codegas.commons.lang.value;
 
+import java.security.Principal;
 import java.util.Objects;
 
 public final class PrincipalName extends AbstractId {
@@ -8,6 +9,10 @@ public final class PrincipalName extends AbstractId {
 
     public static PrincipalName fromString(String string) {
         return string == null ? null : new PrincipalName(string);
+    }
+
+    public static PrincipalName fromPrincipal(Principal principal) {
+        return principal == null ? null : new PrincipalName(principal.getName());
     }
 
     public PrincipalName(String value) {
